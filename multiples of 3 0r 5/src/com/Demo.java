1e093a9de8c3e51842d58a6e436673ef285fe6b1
malavikaharikumar23 <malavikaharikumar.mec@gmail.com>
2018-05-02 23:16:47 +0530

    Second commit

---
+++ multiples of 3 0r 5/src/com/Demo.java
@@ -0,0 +1,21 @@
+package com;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Sum of  all multiples of 3 or 5 below 1000 is " +sumOfmultiples(1000));
+	}
+public static int sumOfmultiples(int limit)
+{
+	int i,sum=0;
+	for(i=3;i<limit;i++)
+	{
+		if(i%3==0 || i%5==0)
+		{
+			sum+=i;
+		}
+	}
+	return sum;
+}
+}
